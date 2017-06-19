0780b3120ffbf22845f701b61e396731d3e9ef9e
Nimrod Dickler <nimroddickler@gmail.com>
2017-06-19 20:15:33 +0300

    working server

---
+++ Labs_301860698_201299625/Lab6_TCPMessengerClient/Lab6TCPMessengerClient/Debug/src/TCPMessengerClient.d
@@ -0,0 +1,13 @@
+src/TCPMessengerClient.d src/TCPMessengerClient.o: \
+ ../src/TCPMessengerClient.cpp ../src/TCPMessengerClient.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/MThread.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/TCPSocket.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/TCPMessengerProtocol.h
+
+../src/TCPMessengerClient.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/MThread.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/TCPSocket.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/TCPMessengerProtocol.h:
---
+++ Labs_301860698_201299625/Lab6_TCPMessengerClient/Lab6TCPMessengerClient/Debug/src/main.d
@@ -0,0 +1,12 @@
+src/main.d src/main.o: ../src/main.cpp ../src/TCPMessengerClient.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/MThread.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/TCPSocket.h \
+ /home/efi/Dropbox/workspace/msocketsLib/include/TCPMessengerProtocol.h
+
+../src/TCPMessengerClient.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/MThread.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/TCPSocket.h:
+
+/home/efi/Dropbox/workspace/msocketsLib/include/TCPMessengerProtocol.h:
